6867d77e40a0be48acab4278a3dbbce4f43111a8
lucasraulinosilva <lraulino26@gmail.com>
2024-11-14 21:56:17 -0300

    v6

--- src/main/java/med/voll/api/domain/usuario/UsuarioRepository.java
+++ src/main/java/med/voll/api/domain/usuario/UsuarioRepository.java
@@ -1,6 +1,7 @@
 package med.voll.api.domain.usuario;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
