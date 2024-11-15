3d6055d56367249b3224023b406dd0a1142aa67b
lucasraulinosilva <lraulino26@gmail.com>
2024-11-14 21:48:51 -0300

    v4

--- src/main/java/med/voll/api/domain/usuario/UsuarioRepository.java
+++ src/main/java/med/voll/api/domain/usuario/UsuarioRepository.java
@@ -1,9 +1,6 @@
 package med.voll.api.domain.usuario;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
